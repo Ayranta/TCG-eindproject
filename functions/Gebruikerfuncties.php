43bf3aac203242b5d04d25e6ae7a129fd165b18c
Ayranta <casper.nauwelaerts@gmail.com>
2023-10-19 15:20:45 +0200

    add basics

---
+++ functions/Gebruikerfuncties.php
@@ -0,0 +1,37 @@
+<?php
+
+function isEmailCorrect($connection,$email){
+    $resultaat = $connection->query("SELECT * FROM tblgebruikers where email = '".$email."'");
+    return ($resultaat->num_rows == 0) ? false : true;
+}
+function isPasswordCorrect($connection,$password,$email){
+    $resultaat = $connection->query("SELECT * FROM tblgebruikers where email = '".$email."'");
+    return (password_verify($password,$resultaat->fetch_assoc()['wachtwoord']))?true:false;
+}
+function registerUser($connection, $fname, $lname, $email, $password, $profile_picture, $desc) {
+    if(empty($profile_picture)) {
+        $profile_picture = "profile.png";
+    }
+
+    $password = convertPasswordToHash($password);
+
+    $resultaat = $connection->query("INSERT INTO tblgebruikers (email, voornaam, naam, wachtwoord, profielfoto, beschrijving) VALUES ('".$email."','".$fname."','".$lname."','".$password."','".$profile_picture."','".$desc."')");
+    return $resultaat;
+}
+function convertPasswordToHash($password) {
+    $hashedpassword = password_hash($password, PASSWORD_DEFAULT);
+    return $hashedpassword;
+}
+function getGebruikersid($connection,$email){
+    $resultaat = $connection->query("SELECT * FROM tblgebruikers where email = '".$email."'");
+    return ($resultaat->num_rows == 0)?false:$resultaat->fetch_assoc()['gebruikerid'];
+}
+function checkIfAdmin($connection,$email){
+    $resultaat = $connection->query("SELECT * FROM tblgebruikers where email = '".$email."' and admin=1");
+    return ($resultaat->num_rows == 0)?false:$resultaat->fetch_all(MYSQLI_ASSOC);
+}
+function getProductUser($connection,$productid){
+    return ($connection->query("SELECT * FROM tblproducten where productid = '".$productid."'"));
+}
+
+?>
