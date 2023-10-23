4f5d8b0b2b3fbf73b118130103fac2522e64fbae
Ayranta <casper.nauwelaerts@gmail.com>
2023-10-23 16:48:57 +0200

    add database

---
+++ database/dbtgcspel.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 23 okt 2023 om 16:48
+-- Serverversie: 10.4.28-MariaDB
+-- PHP-versie: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbtgcspel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `tblgebruikers`
+--
+
+CREATE TABLE `tblgebruikers` (
+  `gebruikerid` int(11) NOT NULL,
+  `email` text NOT NULL,
+  `voornaam` text NOT NULL,
+  `naam` text NOT NULL,
+  `wachtwoord` text NOT NULL,
+  `admin` tinyint(4) NOT NULL,
+  `status` text NOT NULL,
+  `profielfoto` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
