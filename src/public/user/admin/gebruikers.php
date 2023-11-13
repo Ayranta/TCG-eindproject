587d459cbb84e8f373e5970cb09a70c72f628522
Xander <xnauwelaerts@gmail.com>
2023-11-13 09:34:21 +0100

    final final commit

--- src/public/user/admin/gebruikers.php
+++ src/public/user/admin/gebruikers.php
@@ -1,9 +1,17 @@
 <?php
+
 if (!isset($_SESSION['login'])) {
   header('Location: /account/login');
   exit();
 }
-
+if(!isset($_SESSION['admin'])){
+  header('Location: /');
+  exit();
+}
+if($_SESSION['admin']===0){
+  header('Location: /');
+  exit();
+}
 require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 require_once LIB . '/catalog/users.php';
 
