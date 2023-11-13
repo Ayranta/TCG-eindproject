7c5a7f0f2cd0a5d4e03b99d6245b831a924064a0
Xander <xnauwelaerts@gmail.com>
2023-11-13 16:23:04 +0100

    initial commit

---
+++ src/public/user/admin/categorieen.php
@@ -0,0 +1,39 @@
+
+<?php 
+
+$categorieen = fetchSingle('SELECT * FROM `kaart_categorieen`');
+var_dump($categorieen);
+
+?>
+<a href="/dashboard/categorieen/add"><button class="btn btn-active">voeg nieuwe categorieen toe</button></a>
+<?php
+
+foreach($categorieen as $categorie){
+?>
+<div class="flex">
+    <div class="overflow-x-auto flex[1.8]">
+    <table class="table table-lg">
+        <!-- head -->
+        <thead>
+        <tr>
+            <th>id</th>
+            <th>Name</th>
+            <th>color</th>
+            <th>wijzigen</th>
+        </tr>
+        </thead>
+        <tbody>
+        <!-- row 1 -->
+        <tr>
+            <th><?php echo $categorie['id'] ?></th>
+            <td><?php echo $categorie['naam'] ?></td>
+            <td class=""><span class="shadow rounded-full bg-red p-2 bg-black"><?php echo $categorie['kleur hexadeximaal'] ?></span></td>
+            <td ><a><button class="btn btn-primary">wijzigen</button></a></td>
+            <td ><a><button class="btn btnc-red">delete</button></a></td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
+    <div class="flex[0.3]"></div>
+</div>
+<?php } ?>
