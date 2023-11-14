ad70494ad8e54a2719128efd8e9e80e58e8fb3d5
Xander <xnauwelaerts@gmail.com>
2023-11-14 11:33:58 +0100

    update database

--- src/database/seed/dbtgcspel.sql
+++ src/database/seed/dbtgcspel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 25 okt 2023 om 09:19
+-- Gegenereerd op: 14 nov 2023 om 11:32
 -- Serverversie: 10.4.28-MariaDB
 -- PHP-versie: 8.2.4
 
@@ -24,6 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabelstructuur voor tabel `kaart_categorieen`
+--
+
+CREATE TABLE `kaart_categorieen` (
+  `id` int(11) NOT NULL,
+  `naam` text NOT NULL,
+  `kleur hexadeximaal` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `kaart_categorieen`
+--
+
+INSERT INTO `kaart_categorieen` (`id`, `naam`, `kleur hexadeximaal`) VALUES
+(1, 'fire', 'ff2a04');
+
+-- --------------------------------------------------------
+
+--
 -- Tabelstructuur voor tabel `tblgebruikers`
 --
 
@@ -40,7 +59,8 @@ CREATE TABLE `tblgebruikers` (
 
 INSERT INTO `tblgebruikers` (`gebruikerid`, `email`, `wachtwoord`, `gebruikernaam`) VALUES
 (12, 'bobdejef@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$UzJhYjByRnBQLlZlcTcyRA$AtAMrnpax5kLsESYTKkkLtxXLPFXURrbMgCtmD7KFfA', 'xandanman'),
-(13, 'jaaaaaaasper@hotmail.com', '$argon2id$v=19$m=65536,t=4,p=1$SHdmWWNzTnF4MDJzbVM3UA$5rjdR6zjIplOrjLY0eDIVeSrbq3ofkTn2dG0iluSWR0', 'xander');
+(13, 'jaaaaaaasper@hotmail.com', '$argon2id$v=19$m=65536,t=4,p=1$SHdmWWNzTnF4MDJzbVM3UA$5rjdR6zjIplOrjLY0eDIVeSrbq3ofkTn2dG0iluSWR0', 'xander'),
+(14, 'aaa@hotmail', '$argon2id$v=19$m=65536,t=4,p=1$ckliWjcvLzhXVEtzZmRzUA$CX5nuYBc4iXJul5V3otLOH7H3BtfntYQ7Q7ZqpsEjDU', 'test2');
 
 -- --------------------------------------------------------
 
@@ -62,11 +82,18 @@ CREATE TABLE `tblgebruiker_profile` (
 
 INSERT INTO `tblgebruiker_profile` (`id`, `userid`, `theme`, `profielfoto`, `admin`) VALUES
 (9, 12, 'light', 'https://avatars.githubusercontent.com/u/64209400?v=4', 0),
-(10, 13, 'light', 'https://avatars.githubusercontent.com/u/64209400?v=4', 0);
+(10, 13, 'light', 'https://avatars.githubusercontent.com/u/64209400?v=4', 0),
+(11, 14, 'light', 'https://avatars.githubusercontent.com/u/64209400?v=4', 0);
 
 --
 -- Indexen voor geëxporteerde tabellen
 --
+
+--
+-- Indexen voor tabel `kaart_categorieen`
+--
+ALTER TABLE `kaart_categorieen`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexen voor tabel `tblgebruikers`
@@ -85,16 +112,22 @@ ALTER TABLE `tblgebruiker_profile`
 --
 
 --
+-- AUTO_INCREMENT voor een tabel `kaart_categorieen`
+--
+ALTER TABLE `kaart_categorieen`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT voor een tabel `tblgebruikers`
 --
 ALTER TABLE `tblgebruikers`
-  MODIFY `gebruikerid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `gebruikerid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT voor een tabel `tblgebruiker_profile`
 --
 ALTER TABLE `tblgebruiker_profile`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
