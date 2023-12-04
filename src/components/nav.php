ec3192892cd46aa7cf5ead03f70a52ead3539403
Xander <xnauwelaerts@gmail.com>
2023-12-04 08:36:47 +0100

    added level display

--- src/components/nav.php
+++ src/components/nav.php
@@ -113,7 +113,14 @@ $GroupLevelofPlayer=fetch('SELECT * From LevelGroup Where GroupID = ?',[
  
  
   <div class="navbar-end">
-
+  <?php if ($user) {?>
+  <div class="flex items-center space-x-2 mr-8">
+      <span class="">Level:</span>
+      <div class="bg-[#CD7F32] rounded-full h-8 w-8 flex items-center justify-center">
+          <span class=" text-sm font-semibold"><?php echo $change_theme['Level'] ?></span>
+      </div>
+  </div>
+<?php }?>
     <?php if ($yourfriendrequest){ ?>
   <div class="alert shadow-lg flex mx-8" >
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
